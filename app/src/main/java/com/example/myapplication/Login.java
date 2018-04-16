4d016ca9471df18c23b81882204536aa412bd53c
kajolkappe <kappekajol@gmail.com>
2018-04-16 12:29:40 +0530

    Login validation

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -5,22 +5,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class Login extends AppCompatActivity {
 
+    Button log;
+    Button Adminlog;
+    MaterialEditText usrname,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btn = (Button)findViewById(R.id.btncont);
-        btn.setOnClickListener(new View.OnClickListener() {
+        usrname = (MaterialEditText)findViewById(R.id.username);
+        pass = (MaterialEditText)findViewById(R.id.password);
+        log = (Button)findViewById(R.id.btncont);
+        Adminlog = (Button)findViewById(R.id.admin);
+        Adminlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               
+                Intent i = new Intent(getApplicationContext(),adminlogin.class);
+                startActivity(i);
+            }
+        });
+        log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
 
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
-            }
+                String uname = usrname.getText().toString();
+                String pwd = pass.getText().toString();
+                if(usrname.getText().toString().equals("deeptanshu") && pass.getText().toString().equals("dj123"))
+                {
+
+                    Toast.makeText(getApplicationContext(), "Successfully Login", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Enter Correct Username and Password", Toast.LENGTH_SHORT).show();
+                }
+
+                }
         });
     }
 }
